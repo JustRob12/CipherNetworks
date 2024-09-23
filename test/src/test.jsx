8b5bde8278c882df13b45da7eda168f10bd4d37b
just_rob <roberto.prisoris12@gmail.com>
2024-09-23 15:30:22 +0800

    up

--- test/src/test.jsx
+++ test/src/test.jsx
@@ -24,16 +24,16 @@ export function AuroraBackgroundDemo() {
     className="relative flex flex-col gap-4 items-center justify-center px-4"
   >
         {/* Add your logo here */}
-        <img src={cbLogo} alt="CipherNet Logo" style={{ width: "15%", height: "35%" }} />
+        <img src={cbLogo} alt="CipherNet Logo" style={{ width: "20%", height: "45%" }} />
 
-        <div className="text-3xl md:text-7xl font-bold text-center" style={{ color: "#17e0e7" }}>
+        <div className="text-3xl md:text-8xl font-bold text-center" style={{ color: "#17e0e7" }}>
           CipherNet Coming Soon.
         </div>
-        <div className="font-extralight text-center md:text-2xl py-4" style={{ color: "white" }}>
+        <div className="font-extralight text-center md:text-2xl" style={{ color: "white" }}>
           Empowering BSIT students through streamlined management and connected experiences.
         </div>
         <a href="https://www.facebook.com/roberto.prisoris" target="_blank" rel="noopener noreferrer">
-  <button className="bg-white dark:bg-black rounded-full w-fit text-black dark:text-white px-4 py-2">
+  <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-white px-4 py-2">
     Visit Me
   </button>
 </a>
