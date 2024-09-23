2026f95e79b2d4fa9a0d928e8e5f0bc360eb1ab9
just_rob <roberto.prisoris12@gmail.com>
2024-09-23 13:59:43 +0800

    updated

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,12 +1,14 @@
 // App.jsx
 import React from 'react';
 import './index.css';
+import myImage from './my-acetrack-qrcode.jpeg';
 import { ThreeDCardDemo } from './test';
+
 
 function App() {
   return (
     <div>
-      <ThreeDCardDemo />
+      <ThreeDCardDemo imageSrc={myImage} /> {/* Pass the image as a prop */}
     </div>
   );
 }
