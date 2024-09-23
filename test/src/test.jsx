0b36f4638c43798a8915f90f8caac7f75832c3a2
just_rob <roberto.prisoris12@gmail.com>
2024-09-23 15:01:50 +0800

    up

--- test/src/test.jsx
+++ test/src/test.jsx
@@ -7,17 +7,22 @@ import cbLogo from "./assets/cnnet.png";
 
 export function AuroraBackgroundDemo() {
   return (
-    <AuroraBackground className="bg-black dark:bg-neutral-900">
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
+    <AuroraBackground
+  className="bg-black dark:bg-neutral-900 relative overflow-hidden"
+  style={{
+    background: "linear-gradient(360deg, black, #18eaf1)",
+  }}
+>
+  <motion.div
+    initial={{ opacity: 0.0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}
+    className="relative flex flex-col gap-4 items-center justify-center px-4"
+  >
         {/* Add your logo here */}
         <img src={cbLogo} alt="CipherNet Logo" className="w-20 h-20 md:w-40 md:h-40" />
 
@@ -27,9 +32,11 @@ export function AuroraBackgroundDemo() {
         <div className="font-extralight text-base md:text-2xl py-4" style={{ color: "white" }}>
           Empowering BSIT students through streamlined management and connected experiences.
         </div>
-        <button className="bg-white dark:bg-black rounded-full w-fit text-black dark:text-white px-4 py-2">
-          Visit Me
-        </button>
+        <a href="https://www.facebook.com/roberto.prisoris" target="_blank" rel="noopener noreferrer">
+  <button className="bg-white dark:bg-black rounded-full w-fit text-black dark:text-white px-4 py-2">
+    Visit Me
+  </button>
+</a>
       </motion.div>
     </AuroraBackground>
   );
