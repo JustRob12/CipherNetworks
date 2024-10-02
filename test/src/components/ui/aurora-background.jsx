df902ae4d3953065747b8695f8332ea902e59fc4
just_rob <roberto.prisoris12@gmail.com>
2024-10-02 21:38:59 +0800

    updated header

--- test/src/components/ui/aurora-background.jsx
+++ test/src/components/ui/aurora-background.jsx
@@ -12,21 +12,21 @@ export const AuroraBackground = ({
     <main>
       <div
         className={cn(
-          "relative flex flex-col h-[100vh] overflow-y-auto overflow-x-hidden items-center justify-center bg-black text-slate-50 transition-bg",
+          "relative flex flex-col min-h-[100vh] items-center justify-center bg-black text-slate-50 transition-bg",
           className
         )}
         {...props}
       >
-        <div className="absolute inset-0">
+        <div className="absolute inset-0 overflow-hidden">
           <div
             className={cn(
-              ` 
+              `
               [--black-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
               [--aurora:repeating-linear-gradient(100deg,#18e2e9_10%,var(--blue-500)_20%,var(--indigo-400)_30%,var(--blue-300)_40%,#18e2e9_50%)]
+
               [background-image:var(--black-gradient),var(--aurora)]
               [background-size:300%,_200%]
               [background-position:50%_50%,50%_50%]
-              [background-attachment:fixed]
               filter blur-[10px] invert-0
               after:content-[""] after:absolute after:inset-0 after:[background-image:var(--black-gradient),var(--aurora)] 
               after:[background-size:200%,_100%] 
@@ -38,7 +38,7 @@ export const AuroraBackground = ({
             )}
           ></div>
         </div>
-        <div className="z-10 w-full">{children}</div> {/* Ensure children stay on top */}
+        {children}
       </div>
     </main>
   );
